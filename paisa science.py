336c18306a77b9267c183e9d840964b842f2c46c
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2024-10-13 04:05:33 +0530

    Add files via upload

---
+++ paisa science.py
@@ -0,0 +1,15 @@
+a="pizzapizza A=P(1+r/100)^npizzapizza"
+a2="annaanna\\alpha x^2 +\\beta y^3annaanna"
+
+a3="₹₹S=ut+1/2at^2₹₹"
+a5="ramram v=t^3+13+f_9ramram"
+gyro="₹₹d_E=2g_F\\mu_B/2\\eta E \\Delta C₹₹"
+gyro2="paisapaisad_E=(0.3\\pm1.3)\\times 10^{-22}e-cmpaisapaisa"
+a7="HarishHarish S=29+di_{10}+g^5\\betaHarishHarish"
+a6="120₹120₹G=109m^2+9j\\alpha120₹120₹"
+b=a7.replace('Harish','$')
+print(a7)
+print(b)
+
+
+#A code to write any reward and add to the latex equation.. Must be done in 35s or 40s if you don't have job. Who knows what risk is.. lol its funny.
