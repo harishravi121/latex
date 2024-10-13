3075ccf9f2edab6e2dfd734f82b14c203cd2d565
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2024-10-13 14:29:17 +0530

    Add files via upload

---
+++ paisa science2.py
@@ -0,0 +1,24 @@
+a="pizzapizza A=P(1+r/100)^npizzapizza"
+a2="annaanna\\alpha x^2 +\\beta y^3annaanna"
+
+a3="₹₹S=ut+1/2at^2₹₹"
+a5="ramram v=t^3+13+f_9ramram"
+gyro="₹₹d_E=2g_F\\mu_B/2\\eta E \\Delta C₹₹"
+gyro2="paisapaisad_E=(0.3\\pm1.3)\\times 10^{-22}e-cmpaisapaisa"
+a7="HarishHarish S=29+di_{10}+g^5\\betaHarishHarish"
+a6="120₹120₹G=109m^2+9j\\alpha120₹120₹"
+a13="120£120£G=109m^2+9j\\alpha120£120£"
+
+a14="million¢million¢\\beta=192x^3+9m^2+9j\\alphamillion¢million¢"
+a11="lakh₹lakh₹F^{\\mu\\nu}F_{\\mu\\nu}=0lakh₹lakh₹"
+
+a12="crore₹crore₹H ket{\\psi}=E ket{\\psi}crore₹crore₹"
+gluc="billion$billion$C_6H_{12}O_6billion$billion$"
+gluc22="billion$billion$(s_0-\\lambda D)(t_0+\\lambda D)^12billion$billion$"
+import pyperclip as pc
+b=gluc22.replace('billion$','$')
+print(gluc22)
+print(b)
+pc.copy(b)
+
+#A code to write any reward and add to the latex equation.. Must be done in 35s or 40s if you don't have job. Who knows what risk is.. lol its funny.
